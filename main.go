0c2aad235e1de17e919103752a67b3ac5c6ca0ec
Andre Betiolo <andrebetiolo@gmail.com>
2021-09-25 11:43:17 -0400

    Get enviroment variables with toolbox env

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"authiny/internal/postgres_embedded"
 	"authiny/internal/storages"
 	"os"
-	"strconv"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/yggbrazil/go-toolbox/env"
@@ -16,15 +15,14 @@ func init() {
 }
 
 func main() {
-	DB_DATABASE := os.Getenv("AUTHINY_DB_DATABASE")
+	DB_HOST := os.Getenv("AUTHINY_DB_HOST")
+	DB_PORT := env.MustInt("AUTHINY_DB_PORT")
 	DB_USER := os.Getenv("AUTHINY_DB_USER")
 	DB_PASSWORD := os.Getenv("AUTHINY_DB_PASSWORD")
-	AUTHINY_DB_FOLDER := os.Getenv("AUTHINY_DB_FOLDER")
+	DB_DATABASE := os.Getenv("AUTHINY_DB_DATABASE")
+	DB_SSLMODE := os.Getenv("AUTHINY_DB_SSLMODE")
 
-	DB_PORT, err := strconv.Atoi(os.Getenv("AUTHINY_DB_PORT"))
-	if err != nil {
-		log.Fatal("Erro ao buscar a porta da variavel: AUTHINY_DB_PORT ", err)
-	}
+	AUTHINY_DB_FOLDER := os.Getenv("AUTHINY_DB_FOLDER")
 
 	postgres := postgres_embedded.NewPostgresEmbedded(postgres_embedded.PostgresEmbeddedConfig{
 		Database: DB_DATABASE,
@@ -34,7 +32,7 @@ func main() {
 		Folder:   AUTHINY_DB_FOLDER,
 	})
 
-	err = postgres.Start()
+	err := postgres.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,12 +40,12 @@ func main() {
 	defer postgres.Stop()
 
 	storage, err := storages.NewPostgresStorage(storages.StoragePostgresConfig{
-		User:     env.String("AUTHINY_DB_USER"),
-		Port:     env.MustInt("AUTHINY_DB_PORT"),
-		Password: env.String("AUTHINY_DB_PASSWORD"),
-		Host:     env.String("AUTHINY_DB_HOST"),
-		DataBase: env.String("AUTHINY_DB_DATABASE"),
-		SSLMode:  env.String("AUTHINY_DB_SSLMODE"),
+		User:     DB_USER,
+		Port:     DB_PORT,
+		Password: DB_PASSWORD,
+		Host:     DB_HOST,
+		DataBase: DB_DATABASE,
+		SSLMode:  DB_SSLMODE,
 	})
 	if err != nil {
 		log.Fatal(err)
